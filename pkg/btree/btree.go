25756a4134f6fe7efa9adccff652076713840253
sixletters <e0406779@u.nus.edu>
2023-08-02 00:38:11 +0800

    items

--- pkg/btree/btree.go
+++ pkg/btree/btree.go
@@ -1,1 +1,14 @@
 package btree
+
+// consts "sixletters/kv-store/pkg/consts"
+
+type Node struct {
+	ItemList []*Item
+	children []*Node
+}
+
+type BTree struct {
+	Root     *Node
+	minItems int
+	maxItems int
+}
--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -1,1 +1,8 @@
 package consts
+
+const (
+	DefaultMinimumItems    = 128
+	DefaultItemMaxSize     = 128
+	DefaultItemMaxKeyLen   = 30
+	DefaultItemMaxValueLen = 94
+)
