c64ffd8582879ffc26c0a9bd160f75c2e61fe962
Dion Ng <dionng1998@gmail.com>
2023-11-16 23:34:09 +0800

    set up grpc connection

--- py-sdk-client/__init__.py
+++ py-sdk-client/__init__.py
@@ -17,31 +17,31 @@
 
 
 import grpc
-import key_value_pb2
-import key_value_pb2_grpc
+import client_without_internode_pb2
+import client_without_internode_pb2_grpc
 
 class Client:
     def __init__(self, server_address):
         self.channel = grpc.insecure_channel(server_address)
-        self.stub = key_value_pb2_grpc.KeyValueServiceStub(self.channel)
+        self.stub = client_without_internode_pb2_grpc.KeyValueServiceStub(self.channel)
 
     def set_key_value(self, key, value):
-        request = key_value_pb2.KeyValue(key=key, value=value)
-        response = self.stub.SetKeyValue(request)
+        request = client_without_internode_pb2.PutRequest(key=key, value=value)
+        response = self.stub.Put(request)
         return response
 
     def get_value(self, key):
-        request = key_value_pb2.KeyValue(key=key)
-        response = self.stub.GetValue(request)
+        request = client_without_internode_pb2.GetRequest(key=key)
+        response = self.stub.Get(request)
         return response.value
 
 # Usage
 if __name__ == '__main__':
-    server_address = 'localhost:50051'  # Replace with your server's address
+    server_address = 'localhost:8080'  # Replace with your server's address
     client = Client(server_address)
 
-    key = 'example_key'
-    value = 'example_value'
+    key = 'dion'
+    value = 'test_value'
 
     # Set a key-value pair on the server
     response = client.set_key_value(key, value)
