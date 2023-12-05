7bec08669a2e9aa23475565f37bba8c239ae0ce1
Dion Ng <dionng1998@gmail.com>
2023-12-05 23:48:59 +0800

    reformat

--- pkg/server/grpcServer.go
+++ pkg/server/grpcServer.go
@@ -7,20 +7,11 @@ import (
 	"sixletters/simple-db/pkg/tree"
 )
 
-type KeyValueServer struct{
+type grpcServer struct{
 	storageEngine storage.StorageEngine
 }
 
-type notFoundError struct {
-	Key string
-}
-	
-// mustEmbedUnimplementedKeyValueServiceServer implements KeyValueServiceServer.
-func (*KeyValueServer) mustEmbedUnimplementedKeyValueServiceServer() {
-	panic("unimplemented")
-}
-
-func NewKeyValueServer() *KeyValueServer {
+func NewgrpcServer() *grpcServer {
 	newStorageConfig := storage.NewConfigWithOpts(
 		storage.WithTreeType(tree.BTree),
 		storage.WithFilePath("testFile.txt"),
@@ -29,36 +20,25 @@ func NewKeyValueServer() *KeyValueServer {
 	if err != nil {
 		panic(err)
 	}
-	return &KeyValueServer{
+	return &grpcServer{
 		storageEngine: storageEngine,
 	}
 }
 
-func (s *KeyValueServer) Get(ctx context.Context, req *GetRequest) (*GetResponse, error) {
+func (s *grpcServer) Get(ctx context.Context, req *GetRequest) (*GetResponse, error) {
 	res, err := s.storageEngine.Get(context.Background(), req.Key)
 	if err != nil {
 		fmt.Print(err.Error())
 		return nil, err
 	}
 	return &GetResponse{Value: res}, nil
-
-
 }
 
-func (s *KeyValueServer) Put(ctx context.Context, req *PutRequest) (*PutResponse, error) {
+func (s *grpcServer) Put(ctx context.Context, req *PutRequest) (*PutResponse, error) {
 	err := s.storageEngine.Put(context.Background(), req.Key, req.Value)
 	if err != nil {
 		fmt.Print(err.Error())
 		return &PutResponse{Success: false}, err
 	}
 	return &PutResponse{Success: true}, nil
-}
-
-// NotFoundError creates an error indicating that the requested key was not found.
-func NotFoundError(key string) error {
-	return &notFoundError{Key: key}
-}
-
-func (e *notFoundError) Error() string {
-	return "key not found: " + e.Key
 }
